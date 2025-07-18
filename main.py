0eea8cce2245f0caccbfae4eec27e57de22c44a8
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-07-18 12:06:38 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Print the board with horizontal lines
+# Board with positions 1-9
+board = [str(i) for i in range(1, 10)]
+
+win_condition = [[0, 1, 2], [0, 4, 7], [0, 5, 9],[2, 5, 8],
+                    [3, 5, 7],[3, 6, 9], [4, 5, 6], [7, 8, 9]]
+
+
 def welcome():
     print('This is a place to play Tic-Tac-Toe.')
     print('Rules:\n'
@@ -20,12 +28,16 @@ def print_board(board):
         if i < 6:
             print("--+---+--")
 
-# Print the board with horizontal lines
-# Board with positions 1-9
-board = [str(i) for i in range(1, 10)]
+def check_win(board):
+    for condition in win_condition:
+        if all(board[i] == current_player for i in condition):
+            print_board(board=board)
+            print('You won the game!')
+            exit()
+        else:
+            continue
 
-win_condition = [[0, 1, 2], [0, 4, 7], [0, 5, 9],[2, 5, 8],
-                    [3, 5, 7],[3, 6, 9], [4, 5, 6], [7, 8, 9]]
+
 
 
 
@@ -39,16 +51,29 @@ if play_game == 'Y':
 
     while move_count < 9:
         print_board(board=board)
-
+        print(f'move_count is {move_count}')
         try:
             move = int(input(f'Player {current_player}, can you choose a cell? Choose 1-9 : ')) - 1
+
+            if board[move] in ['X', 'O']:
+                move = int(input((f'The cell {move} is already taken.\n'
+                      f'Please choose an empty cell: ')))
+                continue
+
             board[move] = current_player
             move_count += 1
 
-            # Needs to add current_player = 'O'
-            # and let them take turns
-            # check if the user wins
-            # or draw
+            check_win(board=board)
+
+            if move_count == 9:
+                print("It's a draw.")
+                print_board(board=board)
+            else:
+                if current_player == 'X':
+                    current_player = 'O'
+                else:
+                    current_player = 'X'
+
 
         except (ValueError, IndexError):
             print("Warning: Invalid input. Please choose a number from 1 - 9.")
